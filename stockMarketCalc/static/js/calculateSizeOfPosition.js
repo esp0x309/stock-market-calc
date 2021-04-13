ff0d022c02e2f3caa2bacce11db0e038236c5533
esp0x309 <sebstian.nawrot@outlook.com>
2021-04-13 22:07:03 +0200

    Poprawione wyliczenia 'position'

--- stockMarketCalc/static/js/calculateSizeOfPosition.js
+++ stockMarketCalc/static/js/calculateSizeOfPosition.js
@@ -4,19 +4,20 @@ $(document).ready(function() {
         var capital = $("#capital").val();
         var risk_percent = $("#risk_percent").val();
         var risk_money = capital * (risk_percent/100);
+        var reward = $("#reward").val();
         $("#risk_money").val(risk_money);
 
         // Position
         var price = $("#price").val();
+        // sl
         var sl_money = $("#sl_money").val();
-        var sl_percent = price - sl_money/price * 100;
-
-        console.log(price);
-        console.log(sl_money);
-        console.log(sl_percent);
-        var tp_money = $("#tp_money").val();
-        var tp_percent = ((tp_money-price)/price) * 100;
+        var sl_percent = ((price - sl_money)/price) * 100;
         $("#sl_percent").val(sl_percent);
+        // tp
+        var tp_percent = sl_percent * reward;
         $("#tp_percent").val(tp_percent);
+
+        var tp_money = parseFloat(price) + (price * tp_percent/100);
+        $("#tp_money").val(tp_money);
     });
 });
